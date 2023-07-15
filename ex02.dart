b4d72ea1e4481d6e4abea54976888285712afb09
Nycriser <138990317+Nycriser@users.noreply.github.com>
2023-07-15 09:21:51 -0300

    Update ex02.dart

--- ex02.dart
+++ ex02.dart
@@ -1,3 +1,8 @@
 /// Exercício 02 - Contagem Regressiva:
 /// Escreva uma função que utilize um laço de repetição para 
 /// imprimir os números de 10 a 1.
+void main() {
+  for (var i = 10; i > 0; i--) {
+    print(i);
+  }
+}
